932ef2adae5cb96be43fa2ed011d07b4aa959a36
OrangeBird <yuanxinbj@gmail.com>
2024-02-09 17:26:23 +0800

    main
    background is ok

---
+++ src/app/site/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
+        
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10"></div>
+        12
+      </section>
+    </>
+  );
+}
