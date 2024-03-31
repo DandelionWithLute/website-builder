ef0433f8b5416046c353e78f5c78599c7369672d
OrangeBird <yuanxinbj@gmail.com>
2024-03-31 18:52:26 +0800

    main

--- src/providers/editor/editor-providers.tsx
+++ src/providers/editor/editor-providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { EditorBtns } from "@/lib/constants";
 import React from "react";
+import { EditorAction } from "./editor-actions";
 
 export type DeviceTypes = "Desktop" | "Mobile" | "Tablet";
 
@@ -33,28 +34,198 @@ export type EditorState = {
   history: HistoryState;
 };
 
-const initialEditorState: EditorState['editor'] = {
-    elements:[
-        {
-            content:[],
-            id:"__body",
-            name:"Body",
-            styles:{},
-            type:"__body"
-        }
-    ],
-    selectedElement:{
-        id:"",
-        content:[],
-        name:"",
-        styles:{},
-        type:null
+const initialEditorState: EditorState["editor"] = {
+  elements: [
+    {
+      content: [],
+      id: "__body",
+      name: "Body",
+      styles: {},
+      type: "__body",
     },
-    device:"Desktop",
-    previewMode:false,
-    liveMode:false,
-    funnelPageId:""
-}
+  ],
+  selectedElement: {
+    id: "",
+    content: [],
+    name: "",
+    styles: {},
+    type: null,
+  },
+  device: "Desktop",
+  previewMode: false,
+  liveMode: false,
+  funnelPageId: "",
+};
+
+const initialHistoryState: HistoryState = {
+  history: [initialEditorState],
+  currentIndex: 0,
+};
+
+const initialState: EditorState = {
+  editor: initialEditorState,
+  history: initialHistoryState,
+};
+
+const addAnElement = (
+  editorArray: EditorElement[],
+  action: EditorAction
+): EditorElement[] => {
+  if (action.type !== "ADD_ELEMENT") {
+    throw Error(
+      "You sent the wrong action type to the Add Element editor State"
+    );
+  }
+  return editorArray.map((item) => {
+    if (item.id === action.payload.containerId && Array.isArray(item.content)) {
+      return {
+        ...item,
+        content: [...item.content, action.payload.elementDetails],
+      };
+    } else if (item.content && Array.isArray(item.content)) {
+      return {
+        ...item,
+        content: addAnElement(item.content, action),
+      };
+    }
+    return item;
+  });
+};
+
+const updateAnElement = (
+  editorArray: EditorElement[],
+  action: EditorAction
+): EditorElement[] => {
+  if (action.type !== "UPDATE_ELEMENT") {
+    throw Error("You sent the wrong action type to the update Element State.");
+  }
+  return editorArray.map((item) => {
+    if (item.id === action.payload.elementDetails.id) {
+      return { ...item, ...action.payload.elementDetails };
+    } else if (item.content && Array.isArray(item.content)) {
+      return {
+        ...item,
+        content: updateAnElement(item.content, action),
+      };
+    }
+    return item;
+  });
+};
+
+const deleteAnElement = (
+  editorArray: EditorElement[],
+  action: EditorAction
+): EditorElement[] => {
+  if (action.type !== "DELETE_ELEMENT")
+    throw Error(
+      "You sent the wrong action type to the Delete Element editor State"
+    );
+  return editorArray.filter((item) => {
+    if (item.id === action.payload.elementDetails.id) {
+      return false;
+    } else if (item.content && Array.isArray(item.content)) {
+      item.content = deleteAnElement(item.content, action);
+    }
+    return true;
+  });
+};
+
+const editorReducer = (
+  state: EditorState = initialState,
+  action: EditorAction
+): EditorState => {
+  switch (action.type) {
+    case "ADD_ELEMENT":
+      const updatedEditorState = {
+        ...state.editor,
+        elements: addAnElement(state.editor.elements, action),
+      };
+      // Update the history to include the entire updated EditorState
+      const updatedHistory = [
+        ...state.history.history.slice(0, state.history.currentIndex + 1),
+        { ...updatedEditorState }, //Save a copy of the updated state
+      ];
+
+      const newEditorState = {
+        ...state,
+        editor: updatedEditorState,
+        history: {
+          ...state.history,
+          history: updatedHistory,
+          currentIndex: updatedHistory.length - 1,
+        },
+      };
+
+      return newEditorState;
+
+    case "UPDATE_ELEMENT":
+      // Perform your logic to update the element in the state
+      const updatedElements = updateAnElement(state.editor.elements, action);
+
+      const UpdatedElementIsSelected =
+        state.editor.selectedElement.id === action.payload.elementDetails.id;
+
+      const updatedEditorStateWithUpdate = {
+        ...state.editor,
+        elements: updatedElements,
+        selectedElement: UpdatedElementIsSelected
+          ? action.payload.elementDetails
+          : {
+              id: "",
+              content: [],
+              name: "",
+              styles: {},
+              type: null,
+            },
+      };
+
+      const updatedHistoryWithUpdate = [
+        ...state.history.history.slice(0, state.history.currentIndex + 1),
+        { ...updatedEditorStateWithUpdate }, //save a copy of the updated state
+      ];
+      const updatedEditor = {
+        ...state,
+        editor: updatedEditorStateWithUpdate,
+        history: {
+          ...state.history,
+          history: updatedHistoryWithUpdate,
+          currentIndex: updatedHistoryWithUpdate.length - 1,
+        },
+      };
+      return updatedEditor;
+
+    case "DELETE_ELEMENT":
+      // Perform your logic to delete the element from the state
+      const updatedElementsAfterDelete = deleteAnElement(
+        state.editor.elements,
+        action
+      );
+      const updatedEditorStateAfterDelete = {
+        ...state.editor,
+        elements: updatedElementsAfterDelete,
+      };
+      const updatedHistoryAfterDelete = [
+        ...state.history.history.slice(0, state.history.currentIndex + 1),
+        { ...updatedEditorStateAfterDelete }, // save a copy of the updated state
+      ];
+
+      const deleteState = {
+        ...state,
+        editor: updatedEditorStateAfterDelete,
+        history: {
+          ...state.history,
+          history: updatedHistoryAfterDelete,
+          currentIndex: updatedHistoryAfterDelete.length - 1,
+        },
+      };
+      return deleteState;
+
+    case "CHANGE_CLICKED_ELEMENT":
+
+    default:
+      return state;
+  }
+};
 
 const EditorProvider = (props: EditorBtns) => {
   return <div>EditorProvider</div>;
